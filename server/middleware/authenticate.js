387ea34865306c4e1feba2c1a53b7376b2163318
saurabh <saurabh2113076@akgec.ac.in>
2024-08-23 00:06:17 +0530

    fixed file sharing routes

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -3,24 +3,31 @@ const FilePairModel = require("../models/userSchema");
 
 const keysecret = "mynameissaurabhrajputifyouhaveanytyuplkbhkhbkbkjbkknkjjkbjk";
 
-const authenticate = async(req,res,next)=>{
-  try{
-    const token = req.headers.authorization;
-    const verifytoken = jwt.verify(token,keysecret);
-    const rootUser = await FilePairModel.findOne({_id:verifytoken._id})
-    console.log(rootUser);
-    if(!rootUser){throw new Error("user not found")}
-    req.token=token;
-    req.rootUser = rootUser;
-    req.userId = rootUser._id;
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      throw new Error("Authorization token not found");
+    }
+
+    const decoded = jwt.verify(token, keysecret); // Replace "your_secret_key" with your actual secret key
+    const user = await FilePairModel.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.token = token;
+    req.user = user;
+    req.userId = user._id;
     next();
-
-  }catch(error){
-   res.status(401).json({status:401,message:"Unauthorized no token provide"})
-
+  } catch (error) {
+    console.error("Error in authentication middleware:", error);
+    res.status(401).json({ error: "Authentication failed" });
   }
+};
 
-
-} 
-
-module.exports = authenticate
+module.exports = authenticate;
