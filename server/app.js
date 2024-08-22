b255ed48bdec721b2bb246b45d44cb15cb1de40a
saurabh <saurabh2113076@akgec.ac.in>
2024-08-22 23:36:16 +0530

    fixed error cors

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+const dotenv = require("dotenv");
 require("./db/conn");
-const router = require('./routes/router');
+const router = require("./routes/router");
 const cors = require("cors");
-const cookiparser = require("cookie-parser")
-const PORT = process.env.PORT || 3001
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+dotenv.config();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
-app.use(cookiparser());
+app.use(cookieParser());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ignore requests for favicon.ico
+app.get("/favicon.ico", (req, res) => {
+  res.status(404).end();
+});
 app.use(router);
 
-
-app.listen(PORT,()=>{
-  console.log(`server start at port no :${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
